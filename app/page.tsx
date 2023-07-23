4e568681abf76534895b40abfaeb6bb89b22c4ec
u1 <u1.kobayashi@gmail.com>
2023-07-23 22:35:09 +0900

    Temporarily implement data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Header from "@/components/Header";
 import ThreadList from "@/components/ThreadList";
+import { Suspense } from "react";
 
 export default function Index() {
   return (
     <div className="max-w-screen-lg mx-auto">
       <Header />
       <div>
-        <ThreadList categoryId={"categoryId"} />
+        <Suspense fallback={<div>取得中...</div>}>
+          <ThreadList categoryId={"categoryId"} />
+        </Suspense>
       </div>
-    </div>
+    </div >
   )
 }
--- components/ThreadList.tsx
+++ components/ThreadList.tsx
@@ -1,20 +1,38 @@
 import { Thread } from "@/model/Thread";
+import { use } from "react";
 import ThreadListItem from "./ThreadListItem";
 
+type DummyData = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
+
+async function fetchThreads(categoryId: string): Promise<DummyData[]> {
+    return await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then(
+            //ウエイト追加
+            (r) => new Promise((resolve) => setTimeout(() => resolve(r), 3000))
+        );
+}
+
 export default function ThreadList({ categoryId }: { categoryId: string }) {
-    const threads: Thread[] = []
-    for (let i = 0; i < 20; i++) {
-        threads[i] = {
-            threadId: i,
-            categoryId: categoryId,
-            title: "タイトル" + i,
-            commentCount: i,
-            lastUpdatedAt: "2023/07/01 12:00"
-        }
-    }
+    const threads = use(fetchThreads(categoryId))
+        .map((data: DummyData) => {
+            return {
+                threadId: data.id,
+                categoryId: categoryId,
+                title: data.title,
+                commentCount: 0,
+                lastUpdatedAt: "2023-07-01 12:00"
+            }
+        });
+
     return (
         <ul>
-            {threads.map((thread: Thread) => {
+            {threads?.map((thread: Thread) => {
                 return (
                     <li key={thread.threadId} className="border-b"><ThreadListItem thread={thread} /></li>
                 );

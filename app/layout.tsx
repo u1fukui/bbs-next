ebfdc8438499550ad2fa9200f8cdf293da291e31
u1 <u1.kobayashi@gmail.com>
2023-07-23 23:10:11 +0900

    Use Edge runtime for SSR on Cloudflare

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+export const runtime = "edge";
 
 const inter = Inter({ subsets: ['latin'] })
 

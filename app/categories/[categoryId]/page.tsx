51e37562580ec3d61788024139c7c9d79baf5bcc
u1 <u1.kobayashi@gmail.com>
2023-07-25 00:00:32 +0900

    Enable to switch tab

--- app/categories/[categoryId]/page.tsx
+++ app/categories/[categoryId]/page.tsx
@@ -1,14 +1,15 @@
 import Header from "@/components/Header";
 import ThreadCategoryTab from "@/components/ThreadCategoryTab";
 import ThreadList from "@/components/ThreadList";
-import { defaultCategory } from "@/model/Category";
 import { Suspense } from "react";
 
-export default function Index() {
+export default function Index({ params }: {
+  params: { categoryId: string },
+}) {
   return (
     <div className="max-w-screen-lg mx-auto">
       <Header />
-      <ThreadCategoryTab currentCategoryId={defaultCategory.id} />
+      <ThreadCategoryTab currentCategoryId={params.categoryId} />
       <div>
         <Suspense fallback={<div>取得中...</div>}>
           <ThreadList categoryId={"categoryId"} />
--- components/ThreadCategoryTab.tsx
+++ components/ThreadCategoryTab.tsx
@@ -1,30 +1,22 @@
-type Category = {
-    id: string
-    text: string
-}
-
-const categories = [
-    { id: "hot", text: "注目" },
-    { id: "chat", text: "雑談" },
-    { id: "social", text: "世の中" },
-    { id: "life", text: "生活" },
-    { id: "game", text: "アニメ・ゲーム" },
-    { id: "entertainment", text: "エンタメ" },
-    { id: "it", text: "テクノロジー" }
-]
+import { Category, categories, defaultCategory } from "@/model/Category";
+import Link from "next/link";
 
 export default function ThreadCategoryTab({ currentCategoryId }: { currentCategoryId: string }) {
+    if (!categories.some((category) => category.id === currentCategoryId)) {
+        currentCategoryId = defaultCategory.id
+    }
+
     return (
         <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
             <ul className="flex flex-wrap -mb-px">
                 {categories.map((category: Category) => {
                     return (
                         <li key={category.id} className="mr-2">
-                            <a href="#" className={`inline-block p-4 border-b-2 rounded-t-lg
+                            <Link href={"/categories/" + category.id} className={`inline-block p-4 border-b-2 rounded-t-lg
                              ${category.id === currentCategoryId ? "text-blue-600 border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
                                     : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"}`}>
                                 {category.text}
-                            </a>
+                            </Link>
                         </li>
                     );
                 })}
